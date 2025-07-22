88583b60e8504f00898715ddc3412992bd71c3a8
Pumpking-Lord <avivbresca@gmail.com>
2025-07-22 18:51:20 +0300

    Robot basic movment controls

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,36 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.util.sendable.SendableRegistry;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
+/**
+ * This is a demo program showing the use of the DifferentialDrive class, specifically it contains
+ * the code necessary to operate a robot with tank drive.
+ */
+public class Robot extends TimedRobot {
+  CommandXboxController xbox = new CommandXboxController(0);
+  TalonSRX motorRight = new TalonSRX(0);
+  TalonSRX motorLeft = new TalonSRX(0);
+  public void TeleloPeriod(){
+    if(xbox.getLeftY() != 0){
+      //forward and backward
+      motorRight.set(ControlMode.PercentOutput, xbox.getLeftY());
+      motorLeft.set(ControlMode.PercentOutput, xbox.getLeftY());
+    }
+    else if(xbox.getRightX() != 0){
+      //spin left and right
+      motorLeft.set(ControlMode.PercentOutput, xbox.getRightX());
+      motorRight.set(ControlMode.PercentOutput, xbox.getRightX()*-1);
+    }
+  }
+}
